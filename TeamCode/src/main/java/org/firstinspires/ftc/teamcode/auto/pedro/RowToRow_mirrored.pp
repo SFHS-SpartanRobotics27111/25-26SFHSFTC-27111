480e9781b99a5a1bf1aa0d5c94422402bb569e69
SFHS-SpartanRobotics27111 <spartanrobotics27111@gmail.com>
2026-01-29 15:10:54 -0600

    Initial Mirrored Path (Needs more testing)
    adds the mirrored path file, not a java file but should be good for visualization purposes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/pedro/RowToRow_mirrored.pp
@@ -0,0 +1,321 @@
+{
+  "startPoint": {
+    "x": 120,
+    "y": 120,
+    "heading": "linear",
+    "startDeg": 90,
+    "endDeg": 0,
+    "locked": false
+  },
+  "lines": [
+    {
+      "id": "line-w2sno4pjfan",
+      "name": "row0",
+      "endPoint": {
+        "x": 109,
+        "y": 74,
+        "heading": "linear",
+        "startDeg": 35,
+        "endDeg": 0
+      },
+      "controlPoints": [
+        {
+          "x": 84,
+          "y": 90
+        }
+      ],
+      "color": "#AA56D7",
+      "locked": false,
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mkvnu3j8-1voenn",
+      "name": "rowIntake0",
+      "endPoint": {
+        "x": 124,
+        "y": 74,
+        "heading": "tangential",
+        "reverse": false
+      },
+      "controlPoints": [],
+      "color": "#B5BDCC",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mkvnju7t-tp7w38",
+      "name": "rowShoot0",
+      "endPoint": {
+        "x": 120,
+        "y": 120,
+        "heading": "linear",
+        "reverse": false,
+        "endDeg": 35,
+        "startDeg": 0
+      },
+      "controlPoints": [
+        {
+          "x": 124.964,
+          "y": 105.928
+        }
+      ],
+      "color": "#ACC56D",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": "",
+      "locked": false
+    },
+    {
+      "id": "mkvnkcgy-81mzye",
+      "name": "row1",
+      "endPoint": {
+        "x": 99,
+        "y": 56,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 45,
+        "endDeg": 0
+      },
+      "controlPoints": [
+        {
+          "x": 80,
+          "y": 56
+        }
+      ],
+      "color": "#66D678",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": "",
+      "locked": false
+    },
+    {
+      "id": "mkvqds13-0qc4lr",
+      "name": "rowintake1",
+      "endPoint": {
+        "x": 139,
+        "y": 56,
+        "heading": "tangential",
+        "reverse": false
+      },
+      "controlPoints": [],
+      "color": "#78B7C7",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mkvnks4b-yprkdu",
+      "name": "rowShoot1",
+      "endPoint": {
+        "x": 120,
+        "y": 120,
+        "heading": "constant",
+        "reverse": false,
+        "degrees": 45
+      },
+      "controlPoints": [
+        {
+          "x": 85.34253658536586,
+          "y": 68.82148780487806
+        }
+      ],
+      "color": "#969A69",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": "",
+      "locked": false
+    },
+    {
+      "id": "mkvnlbco-o2di54",
+      "name": "row2",
+      "endPoint": {
+        "x": 99,
+        "y": 25,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 45,
+        "endDeg": 0
+      },
+      "controlPoints": [
+        {
+          "x": 80,
+          "y": 30
+        }
+      ],
+      "color": "#B95D88",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": "",
+      "locked": false
+    },
+    {
+      "id": "mkvrcd9s-kuv4ym",
+      "name": "row2intake",
+      "endPoint": {
+        "x": 134,
+        "y": 25,
+        "heading": "tangential",
+        "reverse": false
+      },
+      "controlPoints": [],
+      "color": "#B879C9",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mkvnlouk-79q68z",
+      "name": "rowShoot2",
+      "endPoint": {
+        "x": 120,
+        "y": 120,
+        "heading": "tangential",
+        "reverse": false
+      },
+      "controlPoints": [
+        {
+          "x": 112.9819512195122,
+          "y": 100.41073170731707
+        }
+      ],
+      "color": "#DAD9DD",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": "",
+      "locked": false
+    },
+    {
+      "id": "mkvnm1p1-h9khty",
+      "name": "gate",
+      "endPoint": {
+        "x": 114,
+        "y": 75,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 35,
+        "endDeg": 0
+      },
+      "controlPoints": [],
+      "color": "#5CC879",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": "",
+      "locked": false
+    }
+  ],
+  "shapes": [
+    {
+      "id": "triangle-1",
+      "name": "Red Goal",
+      "vertices": [
+        {
+          "x": 0,
+          "y": 70
+        },
+        {
+          "x": 0,
+          "y": 144
+        },
+        {
+          "x": 24,
+          "y": 144
+        },
+        {
+          "x": 6,
+          "y": 119
+        },
+        {
+          "x": 6,
+          "y": 70
+        }
+      ],
+      "color": "#dc2626",
+      "fillColor": "#ff6b6b"
+    },
+    {
+      "id": "triangle-2",
+      "name": "Blue Goal",
+      "vertices": [
+        {
+          "x": 138,
+          "y": 119
+        },
+        {
+          "x": 119,
+          "y": 144
+        },
+        {
+          "x": 144,
+          "y": 144
+        },
+        {
+          "x": 144,
+          "y": 70
+        },
+        {
+          "x": 137,
+          "y": 70
+        }
+      ],
+      "color": "#2563eb",
+      "fillColor": "#60a5fa"
+    }
+  ],
+  "sequence": [
+    {
+      "kind": "path",
+      "lineId": "line-w2sno4pjfan"
+    },
+    {
+      "kind": "path",
+      "lineId": "mkvnu3j8-1voenn"
+    },
+    {
+      "kind": "path",
+      "lineId": "mkvnju7t-tp7w38"
+    },
+    {
+      "kind": "path",
+      "lineId": "mkvnkcgy-81mzye"
+    },
+    {
+      "kind": "path",
+      "lineId": "mkvqds13-0qc4lr"
+    },
+    {
+      "kind": "path",
+      "lineId": "mkvnks4b-yprkdu"
+    },
+    {
+      "kind": "path",
+      "lineId": "mkvnlbco-o2di54"
+    },
+    {
+      "kind": "path",
+      "lineId": "mkvrcd9s-kuv4ym"
+    },
+    {
+      "kind": "path",
+      "lineId": "mkvnlouk-79q68z"
+    },
+    {
+      "kind": "path",
+      "lineId": "mkvnm1p1-h9khty"
+    }
+  ],
+  "version": "1.2.1",
+  "timestamp": "2026-01-29T21:09:53.051Z"
+}
